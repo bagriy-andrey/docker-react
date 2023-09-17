1b886ef44bf432a01aee0c736d9746a940437910
Andrii Bahrii -X (abahrii - TEAMDEV AMERICA INC at Cisco) <abahrii@cisco.com>
2023-09-17 11:32:55 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test application to deploy
+          Test application to deploy blah-blah
         </p>
         <a
           className="App-link"
